3d502ef94f3dddddbac67d483f20dcaa3e0295b7
jork41989 <jork41989@gmail.com>
2020-09-21 12:56:25 -0400

    finished auth frontend setup

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import Login from "../login/login";
+import { useAuth0 } from "@auth0/auth0-react";
 import './header.css'
+import LogoutButton from "../logout/logout";
 
 
 export default function HeaderBar(){
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  let loginCheck = function(){
+
+    console.log(isAuthenticated)
+    if (isAuthenticated){
+      return (
+        <div>
+          <h3>{user.name}</h3>
+          <LogoutButton/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Login />
+        </div>
+      )
+    }
+  }
 
   return (
     <div className="headerBar">
-      <Login />
+      {loginCheck()}
     </div>
   )
 }
